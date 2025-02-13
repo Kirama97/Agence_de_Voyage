00a9804c233b5d644508c9f6cb370855e61ddc78
KIRAMA97 <officialthiam97@gmail.com>
2025-02-13 17:00:30 +0000

    bug 4

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,9 @@
 
-
+if (typeof WOW !== 'undefined') {
+  new WOW().init();
+} else {
+  console.error("WOW.js n'a pas été chargé correctement.");
+}
 
 const burguer = document.querySelector(".burguer");
 const nav = document.querySelector(".nav_link");
@@ -10,3 +14,4 @@ burguer.addEventListener("click", () => {
   nav.classList.toggle("active");
   burguer.classList.toggle("active");
 });
+
